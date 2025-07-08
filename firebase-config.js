a859b2812ca89ebf02d22dd663e872f512a97cc7
Lev <79760203+CampieGH@users.noreply.github.com>
2025-07-08 15:10:30 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyC-en4T_Vozvrz7o5dyuYRpZ_4j_ACX3pA",
   authDomain: "abobaserver-49923.firebaseapp.com",
   projectId: "abobaserver-49923",
-  storageBucket: "abobaserver-49923.appspot.com",
+  storageBucket: "abobaserver-49923.firebasestorage.app",
   messagingSenderId: "364642279962",
-  appId: "1:364642279962:web:d383373e63e81353d067a3"
+  appId: "1:364642279962:web:d383373e63e81353d067a3",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+export { app, db, storage };
