e9ba78c9f94c8d49dd357bd1337b7a7941efae3f
Lev <79760203+CampieGH@users.noreply.github.com>
2025-07-08 15:18:56 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-en4T_Vozvrz7o5dyuYRpZ_4j_ACX3pA",
   authDomain: "abobaserver-49923.firebaseapp.com",
   projectId: "abobaserver-49923",
-  storageBucket: "abobaserver-49923.firebasestorage.app",
+  storageBucket: "abobaserver-49923.appspot.com",
   messagingSenderId: "364642279962",
-  appId: "1:364642279962:web:d383373e63e81353d067a3",
+  appId: "1:364642279962:web:d383373e63e81353d067a3"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { app, db, storage };
+export { app, db, storage, auth };
